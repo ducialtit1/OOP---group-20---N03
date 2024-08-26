78da6ab8bf6edd816986496fea45106fa2af2921
Duc IT1 K17 <151906230+ducialtit1@users.noreply.github.com>
2024-08-26 21:26:25 +0700

    Add files via upload

---
+++ Tricky Part 2.java
@@ -0,0 +1,19 @@
+public class bai1b {
+	public static class Number{
+		public int i;
+	}
+	
+	public static void main(String[] args) {
+		Number n1 = new Number();
+		Number n2 = new Number();
+		n1.i = 2;
+		 n2.i = 5;
+		 n1 = n2;
+		 n2.i = 10;
+		 n1.i = 20;
+		 System.out.println(n1.i);
+		 System.out.println(n2.i);
+	}
+}
+// n1.i=20
+// n2.i=20
